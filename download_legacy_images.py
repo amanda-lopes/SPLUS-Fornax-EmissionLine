209da1cbffe6b01be74abbcc3fa4a00bacf7c479
Amanda R. Lopes <118303664+amanda-lopes@users.noreply.github.com>
2023-04-26 17:16:13 -0300

    Script to download Legacy images from table
    Creates directory and download images from a table with RA, DEC, radius (in pixels), ID.

---
+++ download_legacy_images.py
@@ -0,0 +1,64 @@
+#Code for creating images from Legacy Survey
+#Written by Amanda Lopes
+
+import argparse
+import wget
+from pathlib import Path
+from astropy.table import Table
+import os
+
+parser = argparse.ArgumentParser(
+    description="""Download images from Legacy""")
+parser.add_argument("table", type=str,
+                    default="table with the sources",
+                    help="Name of table, taken the prefix ")
+
+parser.add_argument("--Object", type=str,
+                    default=None,
+                    help="Id object of a given source ")
+parser.add_argument("--legacy", action="store_true",help="make legacy images")
+
+args = parser.parse_args()
+file_ = args.table + ".ecsv"
+
+try:
+    data = Table.read(file_, format="ascii.ecsv")
+except FileNotFoundError:
+    file_ = args.table + ".csv"
+    data = Table.read(file_, format="ascii")
+
+if args.Object is not None:
+    Object_ = str(args.Object)
+    mask = np.array([source in Object_ for source in data["ID"]])
+    data = data[mask]
+else:
+    data = data
+
+def download_legacy(data,outpath):
+    columns = ["RA","DEC","ID","radius"]
+    for tab in data:
+        try:
+            ra = tab["RA"]
+            dec = tab["DEC"]
+            radii = tab["radius"]
+            Name = tab["ID"]
+        except KeyError as ke:
+            print('No column name {} was found. Please check your table.'.format(ke))
+            os._exit(0)
+
+        print(Name)
+        url = "https://www.legacysurvey.org/viewer/jpeg-cutout?ra=" + str(ra) + "&dec=" + str(dec) + "&size=" + str(
+            radii) + "&layer=ls-dr9&pixscale=0.55&bands=grz"
+        wget.download(url, '{}/{}_{}pix.jpeg'.format(outpath,Name, radii))
+
+if args.legacy==True:
+    dir_output = Path(".")
+    path_legacy = os.path.join(dir_output,'legacy_color_images')
+    if os.path.exists(path_legacy)==False:
+        os.mkdir(path_legacy)
+        print("Directory '{}' created!".format(path_legacy))
+    else:
+        print("Directory '{}' already exists!".format(path_legacy))
+    download_legacy(data,path_legacy)
+    print()
+    print("Download from Legacy Survey finished!")
